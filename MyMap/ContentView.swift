9e1f7a715256a31f01a9f92c6516d3b59f7183bb
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2023-11-17 23:53:13 +0900

    Initial Commit

---
+++ MyMap/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyMap
+//
+//  Created by Hidaka Yoshiki on 2023/11/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyMap/MyMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMapApp.swift
+//  MyMap
+//
+//  Created by Hidaka Yoshiki on 2023/11/17.
+//
+
+import SwiftUI
+
+@main
+struct MyMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
