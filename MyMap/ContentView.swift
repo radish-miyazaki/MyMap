d462c164142d255a9451a9412156e9d8047b678c
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2023-11-18 12:26:18 +0900

    finished reading

--- MyMap/ContentView.swift
+++ MyMap/ContentView.swift
@@ -1,14 +1,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var inputText: String = ""
+    @State private var displaySearchKey: String = "東京駅"
+    @State private var displayMapType: MapType = .standard
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            TextField("キーワード", text: $inputText, prompt: Text("キーワードを入力してください"))
+                .onSubmit {
+                    displaySearchKey = inputText
+                }
+                .padding()
+
+            ZStack(alignment: .bottomTrailing) {
+                MapView(searchKey: displaySearchKey, mapType: displayMapType)
+
+                Button {
+                    if displayMapType == .standard {
+                        displayMapType = .satellite
+                    } else if displayMapType == .satellite {
+                        displayMapType = .hybrid
+                    } else {
+                        displayMapType = .standard
+                    }
+                } label: {
+                    Image(systemName: "map")
+                        .resizable()
+                        .frame(width: 30.0, height: 30.0)
+                        .padding(.trailing, 20.0)
+                        .padding(.bottom, 10.0)
+                }
+            }
         }
-        .padding()
     }
 }
 
--- MyMap/MapView.swift
+++ MyMap/MapView.swift
@@ -1,18 +1,59 @@
-//
-//  MapView.swift
-//  MyMap
-//
-//  Created by Hidaka Yoshiki on 2023/11/17.
-//
-
 import SwiftUI
+import MapKit
+
+enum MapType {
+    case standard
+    case satellite
+    case hybrid
+}
 
 struct MapView: View {
+    let searchKey: String
+    let mapType: MapType
+
+    @State private var targetCoordinate = CLLocationCoordinate2D()
+    @State private var cameraPosition: MapCameraPosition = .automatic
+
+    var mapStyle: MapStyle {
+        switch mapType {
+        case .standard:
+            return MapStyle.standard()
+        case .satellite:
+            return MapStyle.imagery()
+        case .hybrid:
+            return MapStyle.hybrid()
+        }
+    }
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Map(position: $cameraPosition) {
+            Marker(searchKey, coordinate: targetCoordinate)
+        }
+        .mapStyle(mapStyle)
+        .onChange(of: searchKey, initial: true) { oldValue, newValue in
+            let request = MKLocalSearch.Request()
+            request.naturalLanguageQuery = newValue
+
+            let search = MKLocalSearch(request: request)
+
+            search.start { response, error in
+                if let mapItems = response?.mapItems,
+                   let mapItem = mapItems.first {
+                    targetCoordinate = mapItem.placemark.coordinate
+
+                    cameraPosition = .region(
+                        MKCoordinateRegion(
+                            center: targetCoordinate,
+                            latitudinalMeters: 500.0,
+                            longitudinalMeters: 500.0
+                        )
+                    )
+                }
+            }
+        }
     }
 }
 
 #Preview {
-    MapView()
+    MapView(searchKey: "東京駅", mapType: .standard)
 }
