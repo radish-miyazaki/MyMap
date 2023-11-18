4f0801c2e6f36d8655d5591fcfff102b3b145b30
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2023-11-18 12:26:03 +0900

    set AppIcon & add file

---
+++ MyMap/MapView.swift
@@ -0,0 +1,18 @@
+//
+//  MapView.swift
+//  MyMap
+//
+//  Created by Hidaka Yoshiki on 2023/11/17.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapView()
+}
